44fe1af8d5ba2ece8b8e1b009642954fd418fb02
Tue Bui <tuebui2806@gmail.com>
2021-05-04 14:13:09 +0700

    Revert "user-model"
    This reverts commit 1debf5d94b0163ebed735427feac493dbc96d388.

--- src/cybersoft/java11/crm/model/User.java
+++ src/cybersoft/java11/crm/model/User.java
@@ -7,17 +7,16 @@ public class User {
 	private String fullname;
 	private String address;
 	private String phone;
-	private int role;
-	
-	//constructors
+	private Role role;
 	public User(int id, String email, String password, String fullname, String address, String phone) {
+//		super();
 		this.id = id;
 		this.email = email;
 		this.password = password;
 		this.fullname = fullname;
 		this.address = address;
 		this.phone = phone;
-		this.role = -1;
+		this.role = null;
 	}
 	
 	public User() {
@@ -27,72 +26,7 @@ public class User {
 		this.fullname = "";
 		this.address = "";
 		this.phone = "";
-		this.role = -1;
+		this.role = null;
 	}
-	
-	//ToString
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return id + " " + email + " " + password + " " + fullname + " " + address + " " + phone + " " + role;
-	}
-
-	//getters and setters
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFullname() {
-		return fullname;
-	}
-
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public int getRole() {
-		return role;
-	}
-
-	public void setRole(int role) {
-		this.role = role;
-	}
-	
 	
 }
